54f7b91d356254a411305fc4e2dc997ac131259e
dlaczny <lacznypl@hotmail.com>
2026-06-06 13:00:50 +0200

    feat: configure android tv grid density

--- android/app/src/main/java/com/watchlist/tv/WatchlistConfig.java
+++ android/app/src/main/java/com/watchlist/tv/WatchlistConfig.java
@@ -1,6 +1,9 @@
 package com.watchlist.tv;
 
 public final class WatchlistConfig {
+    private static final int MIN_GRID_COLUMNS = 4;
+    private static final int MAX_GRID_COLUMNS = 9;
+
     private WatchlistConfig() {
     }
 
@@ -13,6 +16,20 @@ public final class WatchlistConfig {
             return value.substring(0, value.length() - 1);
         }
 
+        return value;
+    }
+
+    public static int gridColumns() {
+        return clampGridColumns(BuildConfig.WATCHLIST_GRID_COLUMNS);
+    }
+
+    static int clampGridColumns(int value) {
+        if (value < MIN_GRID_COLUMNS) {
+            return MIN_GRID_COLUMNS;
+        }
+        if (value > MAX_GRID_COLUMNS) {
+            return MAX_GRID_COLUMNS;
+        }
         return value;
     }
 }
--- android/app/src/test/java/com/watchlist/tv/WatchlistConfigTest.java
+++ android/app/src/test/java/com/watchlist/tv/WatchlistConfigTest.java
@@ -9,4 +9,13 @@ public class WatchlistConfigTest {
     public void normalizeBaseUrl_removesTrailingSlash() {
         assertEquals("http://10.0.2.2:5000", WatchlistConfig.normalizeBaseUrl("http://10.0.2.2:5000/"));
     }
+
+    @Test
+    public void clampGridColumns_keepsValuesInsideSupportedRange() {
+        assertEquals(4, WatchlistConfig.clampGridColumns(1));
+        assertEquals(4, WatchlistConfig.clampGridColumns(4));
+        assertEquals(7, WatchlistConfig.clampGridColumns(7));
+        assertEquals(9, WatchlistConfig.clampGridColumns(9));
+        assertEquals(9, WatchlistConfig.clampGridColumns(14));
+    }
 }
